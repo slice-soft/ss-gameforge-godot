271215407e89e2b5fbd3885ee8b73c7986e8c2ec
juancadev-io <farfan1976@hotmail.com>
2026-03-27 21:15:47 -0500

    refactor: split monolithic addon into four independent plugins

---
+++ addons/ss-gameforge-singleton/plugin.gd
@@ -0,0 +1,19 @@
+@tool
+extends EditorPlugin
+
+
+func _enable_plugin() -> void:
+	pass
+
+
+func _disable_plugin() -> void:
+	pass
+
+
+func _enter_tree() -> void:
+	pass
+
+
+func _exit_tree() -> void:
+	pass
+
